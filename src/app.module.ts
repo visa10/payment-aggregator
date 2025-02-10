c538633f57db416dc37ab34475ddf4b78f9f72af
valerijmancenko <vsvalera@gmail.com>
2025-02-10 15:55:33 +0200

    fix envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule as EnvConfigModule } from '@nestjs/config';
 import { StoreModule } from './store/store.module';
 import { PaymentModule } from './payment/payment.module';
 import { ConfigModule } from './config/config.module';
@@ -7,11 +8,12 @@ import { PayoutModule } from './payout/payout.module';
 
 @Module({
   imports: [
+    EnvConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST || 'localhost',
       port: Number(process.env.DB_PORT) || 5432,
-      username: process.env.DB_USER || 'valerijmancenko',
+      username: process.env.DB_USER || 'postgres',
       password: process.env.DB_PASSWORD || 'postgres',
       database: process.env.DB_NAME || 'payment_aggregator',
       autoLoadEntities: true,
